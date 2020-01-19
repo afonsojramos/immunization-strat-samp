22c2e8080ca15b32863e4318844917793498bdc7
Afonso Jorge Ramos <up201506239@fe.up.pt>
2020-01-19 07:04:30 +0100

    Final Testing

--- script.r
+++ script.r
@@ -1,6 +1,7 @@
 #### Install ####
 requiredPackages <-
   c("igraph",
+    "svMisc",
     "rstudioapi")
 
 for (pac in requiredPackages) {
@@ -173,6 +174,7 @@ sample_expansion_mcmc <- function(graph, num) {
   return(induced.subgraph(graph, max_nodes))
 }
 
+# Facebook
 
 graph <- graph.fb
 simulate(graph, iter=15, p_i=0.01, p_h=0.9)
@@ -181,49 +183,97 @@ simulate(graph, vac=vac, iter=15, p_i=0.01, p_h=0.9)
 
 
 # Twitter
-simulate(graph.tw, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
 
-# 5 Vaccination Selection
+# 4 Sampling Algorithms
 
-vac_base1 <- rev(order(page.rank(graph.tw)$vector))[1:500]
-sim_vac_base1 <- simulate(graph.tw, vac=vac_base1, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
-
-
-vac_base2 <- sample(1:gorder(graph.tw), 500)
-sim_vac_base2 <- simulate(graph.tw, vac=vac_base2, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
-
-vac_base3 <- rev(order(degree(graph.tw)))[1:500]
-sim_vac_base3 <- simulate(graph.tw, vac=vac_base3, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
-
-vac_base4 <- rev(order(betweenness(graph.tw)$vector))[1:500]
-sim_vac_base4 <- simulate(graph.tw, vac=vac_base4, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
-
-vac_base5 <- rev(order(eigen_centrality(graph.tw)$vector))[1:500]
-sim_vac_base5 <- simulate(graph.tw, vac=vac_base5, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
-
-
-# 4 Samples
+sims_normal <- c()
+sims_mcmc <- c()
+sims_snowball <- c()
+sims_random_node <- c()
+sims_random_edge <- c()
 
 sample_mcmc <- sample_expansion_mcmc(graph.tw, 2000)
-vac1 <- rev(order(page.rank(sample_mcmc)$vector))[1:500]
-simulate(graph.tw, vac=vac1, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
-
-sample_snowball <- sample_expansion_mcmc(graph.tw, 2000)
-vac2 <- rev(order(page.rank(sample_snowball)$vector))[1:500]
-simulate(graph.tw, vac=vac2, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
-
+sample_snowball <- sample_expansion_snowball(graph.tw, 2000)
 sample_random_node <- sample_random_walk_nodes(graph.tw, 2000, mode="ALL")
-vac3 <- rev(order(page.rank(sample_random_node)$vector))[1:500]
-simulate(graph.tw, vac=vac3, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
-
 sample_random_edge <- sample_random_walk_edges(graph.tw, 5000, mode="ALL")
-vac4 <- rev(order(page.rank(sample_random_edge)$vector))[1:500]
-simulate(graph.tw, vac=vac4, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
 
-for (i in 0:15) {  
-  inf <- append(inf, mean(vaccinated[seq(1,160,by=16)+i]))
-  v <- append(v, mean(infected[seq(1,160,by=16)+i]))
+for (i in 0:9) { 
+  vac0 <- rev(order(page.rank(graph.tw)$vector))[1:500]
+  sim_normal <- simulate(graph.tw, vac=vac0, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_normal <- c(sims_normal, sim_normal)
+
+  vac1 <- rev(order(page.rank(sample_mcmc)$vector))[1:500]
+  sim_mcmc <- simulate(graph.tw, vac=vac1, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_mcmc <- c(sims_mcmc, sim_mcmc)
+  
+  vac2 <- rev(order(page.rank(sample_snowball)$vector))[1:500]
+  sim_snowball <- simulate(graph.tw, vac=vac2, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_snowball <- c(sims_snowball, sim_snowball)
+
+  vac3 <- rev(order(page.rank(sample_random_node)$vector))[1:500]
+  sim_random_node <- simulate(graph.tw, vac=vac3, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_random_node <- c(sims_random_node, sim_random_node)
+  
+  vac4 <- rev(order(page.rank(sample_random_edge)$vector))[1:500]
+  sim_random_edge <- simulate(graph.tw, vac=vac4, iter=10, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_random_edge <- c(sims_random_edge, sim_random_edge)
 }
 
-plot(inf, main = "Immunization ")
-points(v, pch=15)
+avg_normal <- c()
+avg_mcmc <- c()
+avg_snowball <- c()
+avg_random_node <- c()
+avg_random_edge <- c()
+
+for (i in 0:10) {  
+  avg_normal <- append(avg_normal, mean(sims_normal[seq(1,110,by=11)+i]))
+  avg_mcmc <- append(avg_mcmc, mean(sims_mcmc[seq(1,110,by=11)+i]))
+  avg_snowball <- append(avg_snowball, mean(sims_snowball[seq(1,110,by=11)+i]))
+  avg_random_node <- append(avg_random_node, mean(sims_random_node[seq(1,110,by=11)+i]))
+  avg_random_edge <- append(avg_random_edge, mean(sims_random_edge[seq(1,110,by=11)+i]))
+}
+
+# 5 Immunization Techniques
+
+sims_pagerank <- c()
+sims_gorder <- c()
+sims_degree <- c()
+sims_betweenness <- c()
+sims_eigen_centrality <- c()
+
+for (i in 0:9) {
+  progress(i)
+  vac_base1 <- order(page.rank(sample_mcmc)$vector)
+  sim_vac_base1 <- simulate(graph.tw, vac=vac_base1, iter=15, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_pagerank <- c(sims_pagerank, sim_vac_base1)
+  
+  vac_base2 <- sample(1:gorder(sample_mcmc), 500)
+  sim_vac_base2 <- simulate(graph.tw, vac=vac_base2, iter=15, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_gorder <- c(sims_gorder, sim_vac_base2)
+  
+  vac_base3 <- order(degree(sample_mcmc))
+  sim_vac_base3 <- simulate(graph.tw, vac=vac_base3, iter=15, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_degree <- c(sims_degree, sim_vac_base3)
+  
+  vac_base4 <- order(betweenness(sample_mcmc))
+  sim_vac_base4 <- simulate(graph.tw, vac=vac_base4, iter=15, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_betweenness <- c(sims_betweenness, sim_vac_base4)
+  
+  vac_base5 <- order(eigen_centrality(sample_mcmc)$vector)
+  sim_vac_base5 <- simulate(graph.tw, vac=vac_base5, iter=15, inf=0.001, p_i=0.005, p_h=0.3)
+  sims_eigen_centrality <- c(sims_eigen_centrality, sim_vac_base5)
+}
+
+avg_pagerank <- c()
+avg_gorder <- c()
+avg_degree <- c()
+avg_betweenness <- c()
+avg_eigen_centrality <- c()
+
+for (i in 0:10) {  
+  avg_pagerank <- append(avg_pagerank, mean(sims_pagerank[seq(1,110,by=11)+i]))
+  avg_gorder <- append(avg_gorder, mean(sims_gorder[seq(1,110,by=11)+i]))
+  avg_degree <- append(avg_degree, mean(sims_degree[seq(1,110,by=11)+i]))
+  avg_betweenness <- append(avg_betweenness, mean(sims_betweenness[seq(1,110,by=11)+i]))
+  avg_eigen_centrality <- append(avg_eigen_centrality, mean(sims_eigen_centrality[seq(1,110,by=11)+i]))
+}
